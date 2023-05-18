395e0961bd4f263c7c370e9ea85d85a7f068e7bc
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-05-18 10:21:34 -0700

    Type correction

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,6 +6,6 @@
     <hr class="!border-t-4" />
     <h2 class="p-4">We're a small team working to build on top of the great work of the USGS, NOAA, and Rec.gov to provide stream flow data and river permit access with an amazing user inteface.</h2>
     <hr class="!border-t-2" />
-    <h3 class="p-4">Find the exact what you need, fast!</h3>
+    <h3 class="p-4">Find exactly what you need, fast!</h3>
     <hr />
 </div>
