72c4907dae492ae7b12fd7131f2f49dcb5a3f70f
chasetmartin <36861079+chasetmartin@users.noreply.github.com>
2023-06-08 11:37:05 -0700

    Error Handling

---
+++ src/routes/+error.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+    import { page } from '$app/stores';
+</script>
+
+<div class="container">
+    {#if $page.status === 404}
+    <h1 class="p-8">You must be lost.</h1>
+    <hr class="!border-t-4" />
+    <div class="w-full items-center justify-center flex p-2">
+    <button class="btn variant-filled-tertiary p-8"><a data-sveltekit-preload-data='off' href="/">Go Home</a></button>
+    </div>
+    <hr />
+    {:else if $page.status === 500}
+    <h1 class="p-8">Whoops we weren't expecting that.</h1>
+    <hr class="!border-t-4" />
+    <h2 class="p-8">Try refreshing the page:</h2>
+    <hr class="!border-t-2" />
+    <div class="w-full items-center justify-center flex p-2">
+    <button class="btn variant-filled-tertiary p-8"><a data-sveltekit-preload-data='off' href="{$page.url.href}"><svg xmlns="http://www.w3.org/2000/svg" width="3em" height="3em" viewBox="0 0 24 24"><g fill="none" stroke="currentColor"><path d="M21 2v6h-6"></path><path d="M3 12a9 9 0 0 1 15-6.7L21 8M3 22v-6h6"></path><path d="M21 12a9 9 0 0 1-15 6.7L3 16"></path></g></svg></a></button>
+    </div>
+    <hr />
+    {/if}
+</div>
--- src/routes/rivers/[slug]/+page.server.ts
+++ src/routes/rivers/[slug]/+page.server.ts
@@ -1,24 +1,38 @@
 import type { PageServerLoad } from './$types';
 import { states } from '$lib/helpers/states';
 
-export const load = (({ params }) => {
-    async function getNowStreamData(stateCode: string | undefined) {
-            const res = await fetch("http://waterservices.usgs.gov/nwis/iv/?stateCd=" + stateCode + "&siteType=ST&siteStatus=active&parameterCd=00060&format=json")
-            const data = await res.json()
-            return data.value
-        }
+export const load: PageServerLoad = async ({ params }) => {
+  try {
+    const stateCode = params.slug;
+    const state = states.find((s) => s.code === stateCode);
 
-        const state = states.find((s) => s.code === params.slug);
+    if (state) {
+      const stateStreams = await getNowStreamData(stateCode);
+      return {
+        stateCode: state.code,
+        stateTitle: state.name,
+        stateStreams: stateStreams
+      };
+    } else {
+      return {
+        stateTitle: 'State Not Found'
+      };
+    }
+  } catch (error) {
+    console.error('An error occurred:', error);
+    return {
+      stateTitle: 'Error Occurred'
+    };
+  }
+};
 
-        if (state) {
-            return {
-                stateCode: state.code,
-                stateTitle: state.name,
-                stateStreams: getNowStreamData(params.slug)
-            };
-        }
-        return {
-            stateTitle: "State Not Found"
-        };
+async function getNowStreamData(stateCode: string | undefined) {
+  const url =
+    'http://waterservices.usgs.gov/nwis/iv/?stateCd=' +
+    stateCode +
+    '&siteType=ST&siteStatus=active&parameterCd=00060&format=json';
 
-}) satisfies PageServerLoad;
+  const res = await fetch(url);
+  const data = await res.json();
+  return data.value;
+}
